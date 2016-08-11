92492e4bd3a1a1e606accc5f3c5e55661f376683
JLChnToZ <jeremylam619@gmail.com>
2016-08-12 00:31:55 +0800

    Change BGA display styles

--- Assets/Scripts/CameraScaler.cs
+++ Assets/Scripts/CameraScaler.cs
@@ -14,9 +14,23 @@ public class CameraScaler : MonoBehaviour {
     }
 
     public float referenceScale = 1;
+    public BMS.Visualization.BMSTextureDisplay[] displays;
 	
 	void LateUpdate () {
+        CalculateMaxScale();
         float scale = (float)Screen.height / Screen.width;
         camera.orthographicSize = scale * referenceScale;
 	}
+
+    void CalculateMaxScale() {
+        if(displays == null || displays.Length < 1) return;
+        float maxScale = float.PositiveInfinity;
+        foreach(var display in displays) {
+            if(!display.isActiveAndEnabled) continue;
+            Vector2 scale = display.transform.localScale;
+            maxScale = Mathf.Min(maxScale, 1 / scale.x);
+        }
+        if(maxScale == float.PositiveInfinity) return;
+        referenceScale = maxScale / 2F;
+    }
 }
