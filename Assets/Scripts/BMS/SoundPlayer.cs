ef44db904bc43417c6bf1b52e68b9f46d701f482
JLChnToZ <jeremylam619@gmail.com>
2016-10-27 00:25:05 +0800

    Fix misuse of volume API

--- Assets/Scripts/BMS/SoundPlayer.cs
+++ Assets/Scripts/BMS/SoundPlayer.cs
@@ -7,10 +7,12 @@ using UnityEngine.Audio;
 
 using ManagedBass;
 using ManagedBass.Fx;
+using ManagedBass.Mix;
 
 namespace BMS {
     public class SoundPlayer: MonoBehaviour {
         static PitchShiftParameters fxParams = new PitchShiftParameters();
+        internal static int mixerHandle;
         bool isAdding;
 
         struct SlicedAudioPlayer {
@@ -29,6 +31,8 @@ namespace BMS {
         Dictionary<int, SlicedAudioPlayer> audioSourceIdMapping = new Dictionary<int, SlicedAudioPlayer>();
         [NonSerialized]
         HashSet<int> unusedAudioSources = new HashSet<int>();
+        [NonSerialized]
+        HashSet<int> addedMixerAudioSources = new HashSet<int>();
 
         [Obsolete("Unused")]
         public AudioMixerGroup mixerGroup;
@@ -46,13 +50,15 @@ namespace BMS {
             get { return volume; }
             set {
                 volume = value;
-                Bass.Volume = value;
+                foreach(SlicedAudioPlayer channel in audioSourceIdMapping.Values)
+                    Bass.ChannelSetAttribute(channel.handle, ChannelAttribute.Volume, value);
             }
         }
 
         static SoundPlayer() {
             if(!Bass.Init(-1, 44100, DeviceInitFlags.Default, IntPtr.Zero, IntPtr.Zero))
                 Debug.LogErrorFormat("Failed to initialize BASS : {0}", Bass.LastError);
+            mixerHandle = BassMix.CreateMixerStream(44100, 2, BassFlags.Default);
         }
 
         public void PauseChanged(bool isPaused) {
@@ -76,6 +82,7 @@ namespace BMS {
         }
 
         void OnApplicationQuit() {
+            mixerHandle = 0;
             Bass.Stop();
             Bass.Free();
         }
@@ -85,12 +92,17 @@ namespace BMS {
                 isAdding = true;
                 audioSourceIdMapping[id] = new SlicedAudioPlayer(handle, sliceStart, sliceEnd);
                 long bytePos = Bass.ChannelSeconds2Bytes(handle, (double)sliceStart.Ticks / TimeSpan.TicksPerSecond);
+                if(!addedMixerAudioSources.Contains(handle)) {
+                    if(BassMix.MixerAddChannel(mixerHandle, handle, BassFlags.Default))
+                        addedMixerAudioSources.Add(handle);
+                }
                 if(bytePos > 0) {
                     Bass.ChannelSetPosition(handle, bytePos);
                     Bass.ChannelPlay(handle, false);
                 } else {
                     Bass.ChannelPlay(handle, true);
                 }
+                Bass.ChannelSetAttribute(handle, ChannelAttribute.Volume, volume);
                 Bass.FXGetParameters(handle, fxParams);
                 fxParams.fPitchShift = pitch;
                 Bass.FXSetParameters(handle, fxParams);
