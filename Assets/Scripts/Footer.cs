d96fa4393adc06da531872c861f8660810cbe702
JLChnToZ <jeremylam619@gmail.com>
2017-08-08 00:10:51 +0800

    Update Version

---
+++ Assets/Scripts/Footer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class Footer : MonoBehaviour {
+
+    private Text text;
+
+    void Awake() {
+        text = GetComponent<Text>();
+        text.text = string.Format(text.text, Application.productName, Application.version);
+    }
+}
