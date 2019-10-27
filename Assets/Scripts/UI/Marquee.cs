edcfa34a3b075b5c73201c5906a4e2bfe54dc45d
JLChnToZ <jeremylam619@gmail.com>
2019-10-28 02:10:17 +0800

    Smaller title font & tweak a bit the marquee

--- Assets/Scripts/UI/Marquee.cs
+++ Assets/Scripts/UI/Marquee.cs
@@ -14,44 +14,43 @@ namespace BananaBeats.UI {
         protected void Awake() {
             rectTransform = GetComponent<RectTransform>();
             if(autoCalculateStopPosition)
-                stopPosition = target.anchoredPosition;
+                stopPosition = target.localPosition;
         }
 
         protected void Update() {
             float t = Time.unscaledDeltaTime;
             var localPos = target.localPosition;
-            var anchoredPos = target.anchoredPosition;
             var pivot = target.pivot;
             var size = target.sizeDelta;
             var parentSize = rectTransform.sizeDelta;
             if(direction == Axis.Horizontal) {
-                if(size.x > parentSize.x) {
+                if(size.x + stopPosition.x > parentSize.x) {
                     if(speed < 0) {
                         if(localPos.x + pivot.x * size.x < -size.x)
-                            anchoredPos.x += parentSize.x + size.x;
+                            localPos.x += parentSize.x + size.x;
                     } else {
                         if(localPos.x + pivot.x * size.x > size.x)
-                            anchoredPos.x -= parentSize.x + size.x;
+                            localPos.x -= parentSize.x + size.x;
                     }
-                    anchoredPos.x += t * speed;
+                    localPos.x += t * speed;
                 } else {
-                    anchoredPos = stopPosition;
+                    localPos = stopPosition;
                 }
             } else {
-                if(size.y > parentSize.y) {
+                if(size.y + stopPosition.y > parentSize.y) {
                     if(speed < 0) {
                         if(localPos.y + pivot.y * size.y < -size.y)
-                            anchoredPos.y += parentSize.y + size.y;
+                            localPos.y += parentSize.y + size.y;
                     } else {
                         if(localPos.y + pivot.y * size.y > size.y)
-                            anchoredPos.y -= parentSize.y + size.y;
+                            localPos.y -= parentSize.y + size.y;
                     }
-                    anchoredPos.y += t * speed;
+                    localPos.y += t * speed;
                 } else {
-                    anchoredPos = stopPosition;
+                    localPos = stopPosition;
                 }
             }
-            target.anchoredPosition = anchoredPos;
+            target.localPosition = localPos;
         }
     }
 }
